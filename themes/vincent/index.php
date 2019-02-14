12a55c401e8344a4cc7f1573ecba2c66f7867f94
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-02-14 09:38:37 -0300

    Integracion de banderas

--- themes/vincent/index.php
+++ themes/vincent/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="container-fluid">
-	<div class="row">
+	<div class="row"><!-- cover -->
 
 		<div class="swiper-container">
 		    <div class="swiper-wrapper">
@@ -12,13 +12,62 @@
 		</div>
 
 
-	</div> <!-- /.row -->
-	<section>
+	</div> <!-- cover -->
+	<section><!-- flags -->
 		<div class="flags">
 			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/flags.png">
 			
 		</div>
-	</section>
+	</section><!-- flags -->
+</div>
+<div class="container">
+	<section> <!-- 6salepoints -->
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-03.png">
+			<h3>Reduzca sus cuentas</h3>
+			<p>Reducción inmediata de su cuenta de luz reflejada en su boleta mensual.</p>
+
+
+		</div>
+
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-05.png">
+			<h3>Aumento de la tarifa</h3>
+			<p>El aumento en la tarifa en su cuenta de la luz ya no será un problema para usted, incluso podria convertir su hogar en un hogar autosustentable.</p>
+
+
+		</div>
+
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-04.png">
+			<h3>Ahorre su dinero</h3>
+			<p>Nuestras soluciones estan pensadas para poder recuperar la inversión en un par de años solamente. 25% de retorno de la inversión al año, por más de 20 años.</p>
+
+
+		</div>
+
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-06.png">
+			<h3>Instalación simple</h3>
+			<p>Instalación simple y rapida, ejecutada por profesionales expertos en el área. Antes de darse cuenta estará produciendo su propia energía.</p>
+
+
+		</div>
+
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-07.png">
+			<h3>Hogar sustentable</h3>
+			<p>Podrás producir tu propia energía lo que permitirá convertir tu hogar en un hogar autosustentable y olvidate de pagar de más.</p>
+
+
+		</div>
+
+		<div class="sale-point text-center">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/snippets/icono-08.png">
+			<h3>Experiencia y calidad</h3>
+			<p>Más de 25 años de experiencia en el rubro solar nos respaldan. Compromiso, profesionalismo, calidad y puntualidad nos caracterizan. </p>
+		</div>		
+	</section><!-- 6salepoints -->
 </div>
 
 <?php get_footer(); ?>
