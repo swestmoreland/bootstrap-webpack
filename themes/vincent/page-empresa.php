5e917f338aa372118ca41e4bad0a0b51ea526da2
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-02-15 17:38:29 -0300

    Boilerplate pagina para empresas

---
+++ themes/vincent/page-empresa.php
@@ -0,0 +1,53 @@
+<?php /* Template Name: Soluciones Empresa */ ?>
+ 
+<?php get_header(); ?>
+<div class="container">
+	<section>
+		<h1>Soluciones Fotovoltaicas para Empresas</h1>
+		<div class="col-sm-12 col-md-12">
+			<img class="img-fluid" src="http://placehold.it/1400x400">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div> 
+
+
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<h2>
+				Galería soluciones fotovoltaicas
+			</h2>
+			<img class="img-fluid" src="http://placehold.it/1400x400">
+		</div>
+	</section>
+	<section>
+		<h1>Soluciones Térmicas</h1>
+		<div class="col-sm-12 col-md-12">
+			<img class="img-fluid" src="http://placehold.it/1400x400">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div> 
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<img class="img-fluid" src="http://placehold.it/500x500">
+		</div>
+
+		<div class="col-xs-12 col-sm-12 col-md-12">
+			<h2>
+				Galería soluciones térmicas
+			</h2>
+			<img class="img-fluid" src="http://placehold.it/1400x400">
+		</div>
+	</section>
+
+
+</div>
+<?php get_footer(); ?>
