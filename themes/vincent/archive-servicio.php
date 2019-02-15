be6b324b4664ed25f50b0a9887ad7940b891f1ea
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-02-15 12:55:57 -0300

    Modificacion global de h1

--- themes/vincent/archive-servicio.php
+++ themes/vincent/archive-servicio.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
+<div class="container-fluid">
+	<div class="row">
+		<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/banners/banner_mantenciones_lg.png">
+	</div>
+</div>
 <div class="container">
 	<section id="manutenciones">
 		
 		<div class="col-sm-12 col-md-12">
-			<h1>Nuestros Servicios</h1>
+			<h1>Instalaciones y Mantenciones</h1>
 		</div>
 		<div class="col-sm-12 col-md-12">
 			<div class="row">
 				<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-					<div class="col-sm-12 col-md-3">
+					<div class="col-sm-12 col-md-6 col-lg-3">
 						<?php if ( has_post_thumbnail() ) : ?>
 							<div class="p-2">
 								<img class="img-fluid" src="<?php the_post_thumbnail_url(); ?>"/>
