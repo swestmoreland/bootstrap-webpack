7eec1be1d6084a9c4cc8384df9cc6b5a493b99ae
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-02-20 12:15:36 -0300

    Soluciones hogar con imagenes reales

--- themes/vincent/page-hogar.php
+++ themes/vincent/page-hogar.php
@@ -3,18 +3,19 @@
 <?php get_header(); ?>
 <div class="container">
 	<section>
-		<h1>Soluciones Fotovoltaicas</h1>
-		<div class="col-sm-12 col-md-12">
-			<img class="img-fluid" src="http://placehold.it/1400x400">
-		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
-		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
-		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
+		<h1>Soluciones energéticas para su hogar</h1>
+		<div class="col-sm-12 col-md-12 py-2">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-banner.png">
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-5 offset-md-1">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-1.png">
+				<p>Sistema fotovoltaico en smarthouse</p>
+			</div> 
+			<div class="col-xs-12 col-sm-12 col-md-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-2.png">
+				<p>Conexión del sistema fotovoltaico 1 fase</p>
+			</div> 
 		</div> 
 
 
@@ -28,16 +29,20 @@
 	<section>
 		<h1>Soluciones Térmicas</h1>
 		<div class="col-sm-12 col-md-12">
-			<img class="img-fluid" src="http://placehold.it/1400x400">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-term-banner.png">
 		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
-		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
-		</div> 
-		<div class="col-xs-12 col-sm-12 col-md-4">
-			<img class="img-fluid" src="http://placehold.it/500x500">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-term-1.png">
+				<p>Sistema de circulación forzada, calefacción + agua caliente</p>
+			</div> 
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-term-2.png">
+				<p>Sistema de circulación forzada, calefacción + agua caliente + piscina</p>
+			</div> 
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-term-3.png">
+			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-12 col-md-12">
