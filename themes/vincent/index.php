d77b4533b196188ea575f4c17f942f9b09b54b52
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-04-11 09:41:42 -0400

    Fix a botonos de soluciones hogar y empresa

--- themes/vincent/index.php
+++ themes/vincent/index.php
@@ -115,10 +115,10 @@ get_header();
 <div class="container-fluid">
 	<section id="soluciones">
 		<div class="col-sm-12 col-md-6" style="cursor: pointer;" onclick="window.location='/soluciones-hogar';">
-			<img class="img-fluid p-2 mx-auto" style="width: 100%" src="<?php echo get_template_directory_uri() ?>/image/snippets/hogar.png">
+			<img class="img-fluid py-1 mx-auto" style="width: 100%" src="<?php echo get_template_directory_uri() ?>/image/snippets/hogar.png">
 		</div>
 		<div class="col-sm-12 col-md-6" style="cursor: pointer;" onclick="window.location='/soluciones-empresa';">
-			<img class="img-fluid p-2 mx-auto" style="width: 100%" src="<?php echo get_template_directory_uri() ?>/image/snippets/empresas.png">
+			<img class="img-fluid py-1 mx-auto" style="width: 100%" src="<?php echo get_template_directory_uri() ?>/image/snippets/empresas.png">
 		</div>
 	</section>
 </div>
