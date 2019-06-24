7f3c87bf24d6117a73e9139b3c7921fd2426dccf
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-06-24 12:17:27 -0400

    Boilerplate preguntas frecuetnes

---
+++ themes/vincent/page-preguntas.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Preguntas frecuentes */ ?>
+ 
+<?php get_header(); ?>
+<div id="questions" class="container">
+	<section>
+		<h1>Preguntas Frecuentes</h1>
+		<div class="col-sm-12 col-md-12 py-2">
+			<img class="img-fluid d-none d-xs-none d-sm-none d-md-block" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-banner.png">
+			<img class="img-fluid d-md-none mx-auto" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-banner-mob.png">
+		</div>
+		
+
+	</section>
+
+</div>
+<?php get_footer(); ?>
