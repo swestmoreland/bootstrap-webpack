79b27a8c65b8e76dcef3bf85a0ddd3cf267043e7
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-07-04 16:30:43 -0400

    Archivos para preguntas frecuentes

--- themes/vincent/page-preguntas.php
+++ themes/vincent/page-preguntas.php
@@ -1,14 +1,18 @@
 <?php /* Template Name: Preguntas frecuentes */ ?>
  
 <?php get_header(); ?>
-<div id="questions" class="container">
-	<section>
-		<h1>Preguntas Frecuentes</h1>
+<div  class="container">
+	<section >
+		<h1> <?php the_title() ?> </h1>
 		<div class="col-sm-12 col-md-12 py-2">
-			<img class="img-fluid d-none d-xs-none d-sm-none d-md-block" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-banner.png">
-			<img class="img-fluid d-md-none mx-auto" src="<?php echo get_template_directory_uri() ?>/image/soluciones/hogar-fv-banner-mob.png">
+			<img style="width:100%" class="img-fluid d-none d-xs-none d-sm-none d-md-block" src="https://via.placeholder.com/750x178">
+			<img style="width:100%" class="img-fluid d-md-none mx-auto" src="https://via.placeholder.com/150">
 		</div>
-		
+		<div id="questions-content" class="col-12">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+			<?php endwhile; endif; ?>
+		</div>
 
 	</section>
 
