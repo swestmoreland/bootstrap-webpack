da02ee0dc682a4f5d9d4f47cf708f4c2f98a3202
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-07-26 10:59:13 -0400

    Ofertas en vertical

--- themes/vincent/index.php
+++ themes/vincent/index.php
@@ -64,29 +64,19 @@ get_header();
 
 		$loop = new WP_Query( $args );
 		?>	
-		<div class="swiper-container movil-swiper d-block d-md-none">
-		    <div class="swiper-wrapper">
-			    <?php
-					while ( $loop->have_posts() ) : $loop->the_post();
-						$image = null;
-							?>
-					      <div class="swiper-slide">
-					      	<a href="<?php echo get_post_permalink(); ?>">
-					      		<img class="img-fluid swiper-lazy" src="<?php echo the_post_thumbnail_url(); ?>">
-					      	</a>
-					      </div>
-							<?php
+	    <?php
+			while ( $loop->have_posts() ) : $loop->the_post();
+				$image = null;
+					?>
+		      		<a href="<?php echo get_post_permalink(); ?>">
+		      		<img class="img-fluid w-100" src="<?php echo the_post_thumbnail_url(); ?>">
+		      		</a>
+					<?php
 
-					endwhile;
-					wp_reset_postdata();
-			        wp_reset_query();
-				?>
-		    </div>
-		    <!-- If we need navigation buttons -->
-		    <div class="swiper-button-prev"></div>
-		    <div class="swiper-button-next"></div>		    
-		</div>
-
+			endwhile;
+			wp_reset_postdata();
+	        wp_reset_query();
+		?>
 
 	</div> <!-- cover -->
 	<section><!-- SECOND TO MAIN COVERS -->
