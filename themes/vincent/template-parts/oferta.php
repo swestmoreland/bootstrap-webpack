d36831c8515d762993448271b36db4b39aa9eecd
Marcelo Barrientos <marcelo@vincentsolar.com>
2019-08-08 09:38:21 -0400

    boton contactenos oferta y nuevos colores de empresa

--- themes/vincent/template-parts/oferta.php
+++ themes/vincent/template-parts/oferta.php
@@ -5,14 +5,14 @@
 	<div class="col-sm-12 col-md-10 offset-md-1">
 		<div id="<?php if($hash) {echo $hash;} ?>"></div>
 		<div class="row">
-			<div class="col-sm-12 col-md-6 col-lg-5">
+			<div class="col-sm-12 col-md-6 col-lg-5 col-xl-6">
 				<?php if ( has_post_thumbnail() ) : ?>
 					<div class="py-2">
 						<img class="img-fluid" src="<?php the_post_thumbnail_url(); ?>"/>
 					</div>
 				<?php endif; ?>
 			</div>
-			<div class="col-sm-12 col-md-6 col-lg-7 oferta-body">
+			<div class="col-sm-12 col-md-6 col-lg-7 col-xl-6 oferta-body">
 				<h2 class="px-2"><?php the_title() ;?> <small>Todo Incluido</small></h2>
 				<?php the_content() ;?>
 				<a class="boton-cotizar mov" href="mailto:info@vincentsolar.com?subject=Intersado en <?php the_title() ;?>&body=Nombre:%0D%0ACiudad:%0D%0ATelefono:%0D%0AComentario:">
